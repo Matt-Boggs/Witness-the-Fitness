cdc98fc5374da24acaa6c160f6ce247445bb72fa
Matt Boggs <matt.david.boggs@gmail.com>
2020-11-13 15:34:24 -0700

    matching routes to api.js

---
+++ routes/api-routes.js
@@ -0,0 +1,21 @@
+const db = require('../models')
+
+module.exports = (app)=>{
+
+    app.get("/api/workouts", (req,res)=>{
+        db.Workout.find({}, (err,workouts)=>{
+            if(err){
+                console.log(err)
+            } else {
+                res.json(workouts)
+            }
+        })
+    })
+
+    app.put("/api/workouts/:id", (req,res)=>{
+        
+    })
+
+
+
+}
