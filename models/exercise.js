d6d54770b84d29557f1b63e8d9b9dd0691bc6535
Matt Boggs <matt.david.boggs@gmail.com>
2020-11-13 15:15:45 -0700

    models based on seeder

--- models/exercise.js
+++ models/exercise.js
@@ -2,7 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
-
+    type: String,  // this might be a problem
+    name:{
+        type: String,
+        unique: true
+    },
+    distance: Number,
+    duration: Number,
+    weight: Number,
+    sets: Number,
+    reps: Number
 })
 const Exercise = mongoose.model("Exercise", ExerciseSchema);
 
--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    
+    day: {
+        type: Date,
+        default: Date.now() // this is set later on
+    },
+    exercises: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Exercise"
+        }
+    ] 
 })
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
