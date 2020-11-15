35a4ed42384730a70d3c0c495a2bf2f4d4cf2d24
Matt Boggs <matt.david.boggs@gmail.com>
2020-11-15 12:05:59 -0700

    work on routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,21 +3,25 @@ const db = require('../models')
 module.exports = (app)=>{
 
     app.get("/api/workouts", (req,res)=>{
-        db.Workout.find({}, (err,workouts)=>{
-            if(err){
-                console.log(err)
-            } else {
-                res.json(workouts)
-            }
+        db.Workout.find({})
+        .populate("exercises")
+        .then(dbWorkout=>{
+            console.log(dbWorkout)
+            res.json(dbWorkout)
+        })
+        .catch(err=>{
+            res.json(err)
         })
     })
 
-    app.put("/api/workouts/:id", (req,res)=>{
-
-    })
-
-    app.post('/api/workouts', (req,res) => {
+    // app.put("/api/workouts/:id", (req,res)=>{
         
+    // })
+
+    app.post("/api/workouts", (req,res) => {
+        db.Workout.create({}).then(newWorkout => {
+            res.json(newWorkout);
+        });
     });
 
 
