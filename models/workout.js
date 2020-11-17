76921ca9311660fbf1710f9de0c1ce66a69fc3da
Matt Boggs <matt.david.boggs@gmail.com>
2020-11-17 09:23:55 -0700

    fleshing out model

--- models/workout.js
+++ models/workout.js
@@ -7,12 +7,36 @@ const WorkoutSchema = new Schema({
         default: Date.now() // this is set later on
     },
     exercises: [
+        // {
+        //     type: Schema.Types.ObjectId,
+        //     ref: "Exercise"
+        // }
         {
-            type: Schema.Types.ObjectId,
-            ref: "Exercise"
-        }
+            type: {
+              type: String,
+              trim: true
+            },
+            name: {
+              type: String,
+              trim: true
+            },
+            distance: Number,
+            duration: Number,
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            performed: Boolean
+          }
+        
     ] 
-})
+},{
+  toJSON: {virtuals: true}
+});
+
+WorkoutSchema.virtual("totalDuration").get(()=>{
+  return this.exercises.reduce((acc,curVal)=> acc + curVal.duration, 0)
+});
+
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
 
